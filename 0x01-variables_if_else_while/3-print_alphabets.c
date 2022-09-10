058a5e087769ac6811465a41eba105b90418dc1d
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-10 03:41:53 -0700

    2-print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the alphabet in lowercase and uppercase
  * Description:'This program alphabet in lower and uppercase
- * 		followed by a new line'
+ * followed by a new line'
  * Return: Always 0 (Success)
  */
 int main(void)
