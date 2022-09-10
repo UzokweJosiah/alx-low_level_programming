58fa9bd8029a0a74466835bfeb6f2f3dc2b87058
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-09 17:04:30 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main(void){
-
+int main(void)
+{
 	int alphabet;
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
+		putchar('\n');
 	return (0);
 }
