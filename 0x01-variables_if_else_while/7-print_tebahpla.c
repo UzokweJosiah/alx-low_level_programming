dd8f889cc485cbe53ce4916cb5083b2f87cb22cf
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-10 05:44:20 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower cass alphabet in reverse
+ *
+ * Description:'This program prints the lowercase alphabet in
+ * reverse, followed by a new line'
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+
+}
