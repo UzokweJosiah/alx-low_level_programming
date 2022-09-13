23a45928416b40d3e01a1d3705d28e98a3f598a9
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-13 15:05:05 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+include "main.h"
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Description:'A function that prints alphabet in lowercase followed ny newline'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+
+}
