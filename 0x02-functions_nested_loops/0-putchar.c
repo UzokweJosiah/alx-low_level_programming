46aa29366ebb2aa5d8fb723faa0f9fd2ddbaaf87
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-13 14:21:40 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - writes the charavter c to stdout
+ *
+ * Description:'This program prints _putchar followed
+ * by a new line and return 0'
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+
+}
