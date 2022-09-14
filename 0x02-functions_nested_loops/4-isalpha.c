bbead575659d19552e4c5de214eb438b141a796e
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-14 10:34:00 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * _isalpha - Main Entry
+ *
+ * @c: the character to be check
+ *
+ * Retun: 1 if c is letter, else 0
+ */
+
+int _isalpha(int c);
+{
+	int alphabet;
+	int return_value = 0;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (c == alphabet)
+		{
+			return (1);
+		}
+
+		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+			if (c == alphabet)
+			{
+				return (1);
+			}
+		return (return_value);
+	}
+
+}
