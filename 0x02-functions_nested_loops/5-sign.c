d2a1eb9d480afc9a79908abc52a6b70b7d64e52e
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-14 11:51:39 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdiio.h>
+#include "main.h"
+/**
+ * print_sign - Main entry
+ *
+ * Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+	int num1 = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (num1);
+	}
+
+}
