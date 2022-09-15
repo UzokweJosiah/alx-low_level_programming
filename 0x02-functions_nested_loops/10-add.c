9b971f6c4ea3d00e7fa75b009e560ce670c8f46e
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-14 20:40:49 -0700

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add _ Main Entry
+ *
+ * @n: adds two integars and return result
+ *
+ * Return: rresult
+ */
+int add(int, int)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	num3 = (num1 + num2);
+	return (num3);
+
+}
