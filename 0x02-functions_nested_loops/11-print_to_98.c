5c9116e183ec2ac2085a2b16578ccc03e2edfab6
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-14 20:59:15 -0700

    11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print to 98
+ *
+ * Description: 'the function takes an integer n
+ * and prints it 98.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	for (n <= 98; n++)
+	{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (n >= 98)
+	{
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+	}
+	return (0);
+}
