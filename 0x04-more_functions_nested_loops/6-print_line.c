53522fd49fc39f4d039e4a316664c86d9e453a84
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-15 18:52:55 -0700

    6-print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - main entry
+ *
+ * @n: input arg
+ * Description: the function draw a new line in the terminal
+ *
+ * Return: null
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i <= n;  i++)
+	{
+		_putchar('_');
+
+		if (i <= 0)
+		{
+			_putchar('\n');
+		}
+
+		_putchar('\n');
+	}
+}
