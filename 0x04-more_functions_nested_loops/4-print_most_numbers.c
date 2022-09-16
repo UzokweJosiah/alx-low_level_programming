04cf76454f8252884c5dd934a88581fc9c4a3f7b
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-15 18:03:43 -0700

    4-print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function prints number
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		if (i = 2 || i = 4)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
