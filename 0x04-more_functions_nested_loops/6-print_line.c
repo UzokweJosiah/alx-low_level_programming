107a29a2e8a12d6d50afffde626f58203a1dafe7
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-15 19:07:56 -0700

    6-print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,17 +12,20 @@
 
 void print_line(int n)
 {
-	int i;
 	
-	for (i = 0; i <= n;  i++)
+	if (n <= 0)
 	{
-		_putchar('_');
-
-		if (i <= 0)
-		{
-			_putchar('\n');
-		}
-
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n;  i++)
+		{
+			_putchar('_');
+
+		}
+		_putchar('\n');
+	}
 }
