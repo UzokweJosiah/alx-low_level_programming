8ecff1243d5c96a91069a14955ada654767b9b22
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-15 18:38:05 -0700

    5-more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,13 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	while (j = 0; j <= 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
 		for(i = 0; i <= 14; i++)
 		{
-		_putchar(i);
+			_putchar(i);
 		}
-		_putchar('\n'):
+		_putchar('\n');
 
 	}
 }
