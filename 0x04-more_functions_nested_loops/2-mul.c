358925b30455b49429e7d884c67097f718573740
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-17 21:38:07 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,15 +3,14 @@
 /**
  * mul - function multiplies two integers
  * @a: var 1
- * @b:	var 2
+ * @b: var 2
  * Return: Always 0 (success)
  */
 
 int mul(int a, int b)
 {
 	int num;
-       
+
 	num = a * b;
 	return (num);
-
 }
