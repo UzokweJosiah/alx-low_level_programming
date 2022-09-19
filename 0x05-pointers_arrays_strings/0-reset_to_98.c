e8e502068f08448693f1ab5890025f6f326c0484
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-19 13:52:51 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updatet the value of the variable
+ * the poiter points to 98
+ * @n: pointer to the variable to update
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef FILE_MAAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+void rest_to_98(int *n);
