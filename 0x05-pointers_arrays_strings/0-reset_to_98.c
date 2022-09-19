29524f1307f199a5e91a9b6776c9294e79d44020
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-09-19 14:22:57 -0700

    Readme.md

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,9 @@
  * reset_to_98 - updatet the value of the variable
  * the poiter points to 98
  * @n: pointer to the variable to update
+ *
+ * Description: the function takes apointer to an int as parameter
+ * and update the value it points to.
  */
 void rest_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/* no comments*/
+
+int main(void)
+{
+	int a;
+	int b;
+	
+	a = 98;
+	b = 42;
+
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+
+	return(0)
+}
