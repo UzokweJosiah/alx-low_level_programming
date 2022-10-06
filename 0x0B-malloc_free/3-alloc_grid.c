982fcf7e20c1448e96b180e3133b1ef2abb8b4a7
Uzokwe Josiah <uzokwejosiah@gmail.com>
2022-10-05 20:59:20 -0700

    3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,20 +26,21 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = (int *) malloc(sizeof(int) * width);		
+		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			free(array);
 			for (j = 0; j <= i; j++)
 				free(array[j]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			{
-				array[i][j] = 0;
-			}
+		{
+			array[i][j] = 0;
+		}
 	}
 	return (array);
